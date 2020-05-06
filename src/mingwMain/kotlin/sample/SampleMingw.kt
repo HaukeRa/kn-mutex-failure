d8539decbce659db47070e94a2cee1bd12f1c244
Hauke Radtki <haukeradtki@gmail.com>
2020-05-06 17:17:09 +0200

    try on kotlin 1.4-M1 (still fails)

--- src/mingwMain/kotlin/sample/SampleMingw.kt
+++ src/mingwMain/kotlin/sample/SampleMingw.kt
@@ -1,21 +1,33 @@
 package sample
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.Semaphore
 import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.sync.withPermit
 
+private suspend fun Semaphore.lock() = this.acquire()
+private fun Semaphore.unlock() = this.release()
+
+private suspend fun <T> Semaphore.withLock(action: () -> T): T = this.withPermit(action)
+
+// Fails
+private fun makeMutex() = Mutex()
+
+// Works
+//private fun makeMutex() = Semaphore(1)
+
+const val NUM = 1_000_000
 fun main() {
-    val m = Mutex()
-    GlobalScope.launch(Dispatchers.Default) {
-        m.withLock {
-            println("GlobalScope")
-        }
-    }
+    val m = makeMutex()
+
     runBlocking {
-        repeat(100) {
+        repeat(NUM) {
+            GlobalScope.launch(Dispatchers.Default) {
+                m.withLock {
+                    println("GlobalScope")
+                }
+            }
             m.withLock {
                 println("RunBlocking")
             }
